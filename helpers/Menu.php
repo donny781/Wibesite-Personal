c9152a05cedfa575324f70719ca8001ddc777a51
donny781 <donnysutanto781@gmail.com>
2025-07-09 13:57:28 +0700

    Add files via upload

---
+++ helpers/Menu.php
@@ -0,0 +1,387 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbartopleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'aauth_group_to_group', 
+			'label' => 'Aauth Group To Group', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'aauth_groups', 
+			'label' => 'Aauth Groups', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'aauth_login_attempts', 
+			'label' => 'Aauth Login Attempts', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'aauth_perm_to_group', 
+			'label' => 'Aauth Perm To Group', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'aauth_perm_to_user', 
+			'label' => 'Aauth Perm To User', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'aauth_perms', 
+			'label' => 'Aauth Perms', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'aauth_pms', 
+			'label' => 'Aauth Pms', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'aauth_user', 
+			'label' => 'Aauth User', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'aauth_user_to_group', 
+			'label' => 'Aauth User To Group', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'aauth_user_variables', 
+			'label' => 'Aauth User Variables', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'aauth_users', 
+			'label' => 'Aauth Users', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'agama', 
+			'label' => 'Agama', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'barang', 
+			'label' => 'Barang', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'barang_keluar', 
+			'label' => 'Barang Keluar', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'barang_masuk', 
+			'label' => 'Barang Masuk', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'blog', 
+			'label' => 'Blog', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'blog_category', 
+			'label' => 'Blog Category', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'captcha', 
+			'label' => 'Captcha', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'cc_options', 
+			'label' => 'Cc Options', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'cc_session', 
+			'label' => 'Cc Session', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'crud', 
+			'label' => 'Crud', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'crud_custom_option', 
+			'label' => 'Crud Custom Option', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'crud_field', 
+			'label' => 'Crud Field', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'crud_field_validation', 
+			'label' => 'Crud Field Validation', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'crud_input_type', 
+			'label' => 'Crud Input Type', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'crud_input_validation', 
+			'label' => 'Crud Input Validation', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'data_barang', 
+			'label' => 'Data Barang', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'departemen', 
+			'label' => 'Departemen', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'disposal', 
+			'label' => 'Disposal', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'form', 
+			'label' => 'Form', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'form_custom_attribute', 
+			'label' => 'Form Custom Attribute', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'form_custom_option', 
+			'label' => 'Form Custom Option', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'form_field', 
+			'label' => 'Form Field', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'form_field_validation', 
+			'label' => 'Form Field Validation', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'form_pengajuan_pinjam_barang', 
+			'label' => 'Form Pengajuan Pinjam Barang', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'jabatan', 
+			'label' => 'Jabatan', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'jenis_pengadaan', 
+			'label' => 'Jenis Pengadaan', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'karyawan', 
+			'label' => 'Karyawan', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'kategori', 
+			'label' => 'Kategori', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'keys', 
+			'label' => 'Keys', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'kode_barang', 
+			'label' => 'Kode Barang', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'lokasi', 
+			'label' => 'Lokasi', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'menu', 
+			'label' => 'Menu', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'menu_icon', 
+			'label' => 'Menu Icon', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'menu_type', 
+			'label' => 'Menu Type', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'migrations', 
+			'label' => 'Migrations', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'mutasi', 
+			'label' => 'Mutasi', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'page', 
+			'label' => 'Page', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'page_block_element', 
+			'label' => 'Page Block Element', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'penempatan', 
+			'label' => 'Penempatan', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'pengadaan', 
+			'label' => 'Pengadaan', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'pengajuan', 
+			'label' => 'Pengajuan', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'pengembalian', 
+			'label' => 'Pengembalian', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'rest', 
+			'label' => 'Rest', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'rest_field', 
+			'label' => 'Rest Field', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'rest_field_validation', 
+			'label' => 'Rest Field Validation', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'rest_input_type', 
+			'label' => 'Rest Input Type', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'retur', 
+			'label' => 'Retur', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'ruangan', 
+			'label' => 'Ruangan', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'supplier', 
+			'label' => 'Supplier', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'app_logs', 
+			'label' => 'App Logs', 
+			'icon' => ''
+		)
+	);
+		
+	
+	
+}
